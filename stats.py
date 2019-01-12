22b1c51d7e837d30dc1b392457bf69a12a7951e8
MicheleMorelli <morelli.michele@hotmail.it>
2019-01-12 17:46:11 +0000

    added std deviation and symbols. all tests passing

--- stats.py
+++ stats.py
@@ -3,8 +3,13 @@
 from helper import *
 import re
 import sys
+import math
 
 class Stats:
+    # UTF-compatible math symbols as a reminder for myself:-)
+    variance_sym = "\u03C3\u00B2"
+    standard_deviation_sym = "\u03C3"
+    mean_sym = "\u03BC"
     
     def __init__(self,*args): 
         """Stats class constructor
@@ -17,11 +22,11 @@ class Stats:
             self.n = [int(x) for x in sys.argv[1:]]
         else:
             self.n = get_numeric_series()
-        print("Stats Class initialised")
+        print("Stats Class initialised!")
 
 
     def func_mean(self):
-        """Calculates the mean avg of the n list of numbers
+        """Calculates the arithmetic mean of the n list of numbers
         Returns:
             mean (float): the mean value
         """
@@ -35,22 +40,38 @@ class Stats:
         """
         return sum(map(lambda x: (x - self.func_mean())**2, self.n)) \
                 / len(self.n)
+    
+
+    def func_standard_deviation(self):
+        """Calculates the standard_deviation of the n list of numbers
+        Returns:
+            standard_deviation (float): the std. deviation value
+        """
+        return math.sqrt(self.func_variance())
 
 
     def print_values(self):
         """
         Prints the values of all the stats functions for the numeric list n
         """
-        print("Basic Statistical Functions for numeric series {}"\
-                .format(self.n))
+        div = "\n" + ("*" * 60) + "\n"
+        print( div + "Basic Statistical Functions for numeric series {}"\
+                .format(self.n).upper() + div)
         for attr in dir(self):
             if (not re.match(r'^func_*', attr )): continue
-            func_name = re.sub(r'^func_',"", attr).upper()
-            print("{}: {:.2f}".format(func_name,getattr(self, attr)()))
+            func_name = re.sub(r'^func_',"", attr)
+            sym = getattr(Stats, func_name.lower() + "_sym") 
+            func_name = re.sub(r'_', " ",func_name) # for printing
+            print("{} (symbol: {}): {:.2f}"\
+                    .format(func_name.capitalize(), \
+                    sym,\
+                    getattr(self, attr)()
+                    )
+                    )
 
         
 def main():
-    s = Stats(1,2,3,4,5,6)
+    s = Stats(34,234,23,234,342,34,54)
     s.print_values()
 
 
--- test.py
+++ test.py
@@ -15,4 +15,16 @@ def test_mean2():
 
 def test_variance():
     s = Stats(*range(1,1010))
-    assert s.func_mean() == numpy.var(list(range(1,1010)))  
+    assert s.func_variance() == numpy.var(list(range(1,1010)))  
+
+def test_variance2():
+    s = Stats(*range(1,100))
+    assert s.func_variance() == numpy.var(list(range(1,100)))  
+
+def test_std():
+    s = Stats(*range(1,1010))
+    assert s.func_standard_deviation() == numpy.std(list(range(1,1010)))  
+
+def test_std2():
+    s = Stats(*range(1,100))
+    assert s.func_standard_deviation() == numpy.std(list(range(1,100)))  
