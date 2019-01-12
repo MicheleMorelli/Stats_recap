0c4b29554c553d838c7e20097b61ecc533ba3c29
MicheleMorelli <morelli.michele@hotmail.it>
2019-01-12 18:19:31 +0000

    fixed path for tests

--- test/test.py
+++ test/test.py
@@ -1,3 +1,6 @@
+import sys
+import os
+sys.path.append(os.path.join(sys.path[0],'..','lib'))
 from stats import *
 import numpy
 
@@ -9,22 +12,28 @@ def test_mean():
     s = Stats(*range(1,10))
     assert s.func_mean() == numpy.mean(list(range(1,10)))  
 
+
 def test_mean2():
     s = Stats(*range(1,1010))
     assert s.func_mean() == numpy.mean(list(range(1,1010)))  
+
 
 def test_variance():
     s = Stats(*range(1,1010))
     assert s.func_variance() == numpy.var(list(range(1,1010)))  
 
+
 def test_variance2():
     s = Stats(*range(1,100))
     assert s.func_variance() == numpy.var(list(range(1,100)))  
+
 
 def test_std():
     s = Stats(*range(1,1010))
     assert s.func_standard_deviation() == numpy.std(list(range(1,1010)))  
 
+
 def test_std2():
     s = Stats(*range(1,100))
     assert s.func_standard_deviation() == numpy.std(list(range(1,100)))  
+
