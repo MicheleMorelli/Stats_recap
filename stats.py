ea33ca1e24ddcea8a0939a5f23570faf04655088
MicheleMorelli <morelli.michele@hotmail.it>
2019-01-12 17:17:14 +0000

    added variance func - test failing

--- stats.py
+++ stats.py
@@ -2,6 +2,7 @@
 
 from helper import *
 import re
+import sys
 
 class Stats:
     
@@ -10,13 +11,14 @@ class Stats:
         Takes a series of numeric values from the std input and puts them in
         a list n
         """
-#        if (len(sys.argv) > 1):
-#            self.n = [int(x) for x in sys.argv[1:]]
         if (len(args)):
             self.n = args
+        elif (len(sys.argv) > 1):
+            self.n = [int(x) for x in sys.argv[1:]]
         else:
             self.n = get_numeric_series()
         print("Stats Class initialised")
+
 
     def func_mean(self):
         """Calculates the mean avg of the n list of numbers
@@ -24,6 +26,16 @@ class Stats:
             mean (float): the mean value
         """
         return sum(self.n) / len(self.n)
+   
+
+    def func_variance(self):
+        """Calculates the variance of the n list of numbers
+        Returns:
+            variance (float): the variance value
+        """
+        return sum(map(lambda x: (x - self.func_mean())**2, self.n)) \
+                / len(self.n)
+
 
     def print_values(self):
         """
@@ -31,16 +43,14 @@ class Stats:
         """
         print("Basic Statistical Functions for numeric series {}"\
                 .format(self.n))
-
         for attr in dir(self):
             if (not re.match(r'^func_*', attr )): continue
             func_name = re.sub(r'^func_',"", attr).upper()
             print("{}: {:.2f}".format(func_name,getattr(self, attr)()))
 
         
-
 def main():
-    s = Stats(34,23,4,323,43)
+    s = Stats(1,2,3,4,5,6)
     s.print_values()
 
 
--- test.py
+++ test.py
@@ -12,3 +12,7 @@ def test_mean():
 def test_mean2():
     s = Stats(*range(1,1010))
     assert s.func_mean() == numpy.mean(list(range(1,1010)))  
+
+def test_variance():
+    s = Stats(*range(1,1010))
+    assert s.func_mean() == numpy.var(list(range(1,1010)))  
